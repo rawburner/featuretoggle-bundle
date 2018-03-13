02f7a14b513db7e3fe88375d905732447a4b5d83
Alexander Keil <alexander.keil80@gmail.com>
2018-03-13 09:56:17 +0100

    Cache improvements

--- Entity/FeatureToggleRepository.php
+++ Entity/FeatureToggleRepository.php
@@ -11,23 +11,25 @@ use Doctrine\ORM\EntityRepository;
 class FeatureToggleRepository extends EntityRepository
 {
 
-    protected $cacheFeatures = [];
+    protected $cacheFeatures = null;
     /**
      * Is a feature is enabled
      * @param $feature_name
      * @return bool
      */
     public function isEnabled($feature_name){
+        if($this->cacheFeatures === null){
+            $this->cacheFeatures = [];
+            /** @var FeatureToggle $feature */
+            foreach ($this->findBy(['active' => 1]) as $feature){
+                $this->cacheFeatures[$feature->getFeatureName()] = (bool)$feature->getActive();
+            }
+        }
         if(array_key_exists($feature_name, $this->cacheFeatures)){
             return $this->cacheFeatures[$feature_name];
         }
-        /** @var FeatureToggle $feature */
-        $feature = $this->findOneBy(['feature_name'=>$feature_name]);
-        if(!$feature){
-            return false;
-        }
-        $this->cacheFeatures[$feature_name] = (bool)$feature->getActive();
-        return $this->cacheFeatures[$feature_name];
+        return false;
+
     }
 
 }
