56952afcf490a29af1cd18d536f74a2c9cfc4b32
Alexander Keil <alexander.keil80@gmail.com>
2018-09-11 16:10:14 +0200

    Service definition refactoring

--- Controller/Backend/FeatureToggleController.php
+++ Controller/Backend/FeatureToggleController.php
@@ -6,8 +6,9 @@ use Doctrine\ORM\EntityManager;
 use FeatureToggleBundle\Entity\FeatureToggle;
 use FeatureToggleBundle\Entity\FeatureToggleRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -17,61 +18,9 @@ use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 /**
  * Class FeatureToggleController
  * @package FeatureToggleBundle\Controller\Backend
- * @Route(service="feature_toggle.controller")
  */
-class FeatureToggleController
+class FeatureToggleController extends Controller
 {
-    /**
-     * @var FeatureToggleRepository
-     */
-    private $toggleRepository;
-    /**
-     * @var Router
-     */
-    private $router;
-    /**
-     * @var Session
-     */
-    private $session;
-    /**
-     * @var \Twig_Environment
-     */
-    private $twig_Environment;
-    /**
-     * @var EntityManager
-     */
-    private $entityManager;
-    /**
-     * @var AuthorizationChecker
-     */
-    private $authorizationChecker;
-
-    /**
-     * FeatureToggleController constructor.
-     * @param FeatureToggleRepository $toggleRepository
-     * @param Router $router
-     * @param Session $session
-     * @param \Twig_Environment $twig_Environment
-     * @param EntityManager $entityManager
-     * @param AuthorizationChecker $authorizationChecker
-     */
-    public function __construct(
-        FeatureToggleRepository $toggleRepository,
-        Router $router,
-        Session $session,
-        \Twig_Environment $twig_Environment,
-        EntityManager $entityManager,
-        AuthorizationChecker $authorizationChecker
-
-    )
-    {
-        $this->toggleRepository = $toggleRepository;
-        $this->router = $router;
-        $this->session = $session;
-        $this->twig_Environment = $twig_Environment;
-        $this->entityManager = $entityManager;
-        $this->authorizationChecker = $authorizationChecker;
-    }
 
     /**
      * @param Request $request
@@ -83,26 +32,26 @@ class FeatureToggleController
      * @Route("/feature-toggle", name="feature_toggle")
      */
     public function featureToggleAction(Request $request){
-        if($request->request->has('features')){
-            foreach ($this->entityManager->getRepository(FeatureToggle::class)->findAll() as $featureToggle){
+        if($request->getMethod() == 'POST'){
+            foreach ($this->getDoctrine()->getRepository(FeatureToggle::class)->findAll() as $featureToggle){
                 $featureToggle->setActive(isset($request->request->get('features')[$featureToggle->getId()]));
 
-                if($this->authorizationChecker->isGranted('ROLE_SUPER_ADMIN')){
+                if($this->get("security.authorization_checker")->isGranted('ROLE_SUPER_ADMIN')){
                     $featureToggle->setDescription($request->request->get('description')[$featureToggle->getId()]);
                     $featureToggle->setPublic(isset($request->request->get('public')[$featureToggle->getId()]));
                 }
             }
-            $this->entityManager->flush();
-            $this->session->getFlashBag()->add('notice','Features aktualisiert');
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('notice','Features aktualisiert');
         }
-        if($this->authorizationChecker->isGranted('ROLE_SUPER_ADMIN')){
+        if($this->get("security.authorization_checker")->isGranted('ROLE_SUPER_ADMIN')){
             $filter = [];
         } else{
             $filter = ['public' => 1];
         }
-        $output = $this->twig_Environment->render('@FeatureToggle/Backend/feature_toggle.html.twig',
+        return $this->render('@FeatureToggle/Backend/feature_toggle.html.twig',
             [
-                'features' => $this->toggleRepository->findBy(
+                'features' => $this->getDoctrine()->getRepository(FeatureToggle::class)->findBy(
                     $filter,
                     [
                         'feature_name' => 'asc'
@@ -110,7 +59,6 @@ class FeatureToggleController
                 )
             ]
         );
-        return new Response($output);
     }
 
     /**
@@ -121,11 +69,10 @@ class FeatureToggleController
      * @throws \Doctrine\ORM\OptimisticLockException
      */
     public function deleteFeature($featureToggle){
-        $this->entityManager->remove($featureToggle);
-        $this->entityManager->flush();
-        $this->session->getFlashBag()->add('notice', 'Feature '.$featureToggle->getFeatureName(). ' entfernt');
-        $route = $this->router->generate('feature_toggle');
-        return new RedirectResponse($route);
+        $this->getDoctrine()->getManager()->remove($featureToggle);
+        $this->getDoctrine()->getManager()->flush();
+        $this->addFlash('notice', 'Feature '.$featureToggle->getFeatureName(). ' entfernt');
+        return $this->redirectToRoute('feature_toggle');
 
     }
 
@@ -142,11 +89,10 @@ class FeatureToggleController
                 ->setDescription($request->request->get('feature_description'))
                 ->setFeatureName($request->request->get('feature_name'))
                 ->setActive($request->request->get('feature_active', 0));
-            $this->entityManager->persist($featureToggle);
-            $this->entityManager->flush();
-            $this->session->getFlashBag()->add('notice', 'Feature '.$featureToggle->getFeatureName().' erstellt');
+            $this->getDoctrine()->getManager()->persist($featureToggle);
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('notice', 'Feature '.$featureToggle->getFeatureName().' erstellt');
         }
-        $route = $this->router->generate('feature_toggle');
-        return new RedirectResponse($route);
+        return $this->redirectToRoute('feature_toggle');
     }
 }
